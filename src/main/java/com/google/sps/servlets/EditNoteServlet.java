5d0156c5bb6d1e52fd8d3883b4cb9c099a17a406
Rodrigo Saucedo <rodrisc2000@hotmail.com>
2021-07-27 03:52:34 -0500

    Updates datastore values for a specific entity

---
+++ src/main/java/com/google/sps/servlets/EditNoteServlet.java
@@ -0,0 +1,75 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.Key;
+import com.google.cloud.datastore.KeyFactory;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+import com.google.sps.OAuthUtils;
+import com.google.api.services.oauth2.model.Userinfo;
+import com.google.cloud.datastore.StringValue;
+
+/** Servlet responsible for creating new tasks. */
+@WebServlet("/Edit")
+public class EditNoteServlet extends HttpServlet {
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String sessionId = request.getSession().getId();
+
+    if(OAuthUtils.isUserLoggedIn(sessionId)){
+      Userinfo userInfo = OAuthUtils.getUserInfo(sessionId);
+      String userId = userInfo.getId();
+      String owner = request.getParameter("owner");
+      if(userId.equals(owner)){
+
+        // Sanitize user input to remove HTML tags and JavaScript.
+        String title = Jsoup.clean(request.getParameter("Name"), Whitelist.none());
+        String text = Jsoup.clean(request.getParameter("Text"), Whitelist.none());
+        long id = Long.parseLong(request.getParameter("id"));
+  
+  
+        long timestamp = System.currentTimeMillis();
+  
+        Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+        KeyFactory keyFactory = datastore.newKeyFactory().setKind("Note");
+        Key taskEntityKey = keyFactory.newKey(id);
+        Entity taskEntity =
+            Entity.newBuilder(datastore.get(taskEntityKey))
+                .set("title", title)
+                .set("text", StringValue.newBuilder(text).setExcludeFromIndexes(true).build())
+                .set("timestamp", timestamp)
+                .build();
+        datastore.update(taskEntity);
+  
+        response.sendRedirect("/Notes.html");
+      }else{
+        response.sendError(403);
+        System.out.println("Didn't Errase");
+      }
+    }else{
+      response.sendRedirect("login");
+    }
+  }
+}
